5913992ce5ec417fdeb448e9a97afdbc77299814
jihanejador <jihanejador@gmail.com>
2026-06-03 17:08:47 +0100

    creation function avec la requet de dp

--- src/Repository/SpecialiteRepository.php
+++ src/Repository/SpecialiteRepository.php
@@ -5,6 +5,12 @@ class SpecialiteRepository {
 
     public function __construct($databaseConnection){
         $this->db = $databaseConnection;
-        
+
+    }
+    public function findAll(){
+        $query = "SELECT * FROM specialites";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
 }
