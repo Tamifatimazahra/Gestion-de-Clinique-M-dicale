64859da4b489073e523f02e97dce2d0520531e10
jihanejador <jihanejador@gmail.com>
2026-06-04 11:25:44 +0100

    donnez toue les kpis globales

--- src/Repository/RendezVousRepository.php
+++ src/Repository/RendezVousRepository.php
@@ -6,4 +6,9 @@ class RendezVousRepository {
     public function __construct($databaseConnection){
         $this->db = $databaseConnection;
     }
+    public function getGlobalKPIs(){
+        $q1 = "SELECT ROUND ((COUNT(CASE WHERE statut = 'Annule' THEN 1 END) / COUNT(*)) * 100, 1) AS taux_annulation FROM rendez_vous";
+        $stmt1 = $this->db->query($q1);
+        $res1 = $stmt1->fetch();
+    }
 }
