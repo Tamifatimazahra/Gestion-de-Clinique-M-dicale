f87088704fff07c08ec6c422a7cc5b6c6c0e5b8d
jihanejador <jihanejador@gmail.com>
2026-06-04 11:28:10 +0100

    donnez total de medecins actif

--- src/Repository/RendezVousRepository.php
+++ src/Repository/RendezVousRepository.php
@@ -10,5 +10,9 @@ class RendezVousRepository {
         $q1 = "SELECT ROUND ((COUNT(CASE WHERE statut = 'Annule' THEN 1 END) / COUNT(*)) * 100, 1) AS taux_annulation FROM rendez_vous";
         $stmt1 = $this->db->query($q1);
         $res1 = $stmt1->fetch();
+
+        $q2 = "SELECT COUNT(*) AS total_medecins FROM medecins WHERE actif = TRUE";
+        $stmt2 = $this->db->query($q2);
+        $res2 = $stmt2->fetch();
     }
 }
