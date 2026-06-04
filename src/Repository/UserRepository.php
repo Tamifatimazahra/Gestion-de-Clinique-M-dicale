ae08b76a52453f3bad1e816e2e6fb95a1f034184
jihanejador <jihanejador@gmail.com>
2026-06-04 10:42:07 +0100

    validation sur database

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -38,6 +38,8 @@ class UserRepository{
                 ':id_user' => $idUser,
                 ':id_specialite' => $id_specialite
             ]);
+            $this->db->commit();
+            return true;
         }
     }
 }
