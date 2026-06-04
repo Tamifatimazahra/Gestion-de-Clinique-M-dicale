df883d1f7b66baf9b6ddfc513a2607efb39acb68
jihanejador <jihanejador@gmail.com>
2026-06-04 09:48:46 +0100

    creation la function de donnez tout les medecins avec specialite

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -7,5 +7,14 @@ class UserRepository{
         $this->db = $databaseConnection;
     }
 
-    
+    piblic function findAllMedecins(){
+        $query = "SELECT u.id AS user_id, m.id AS medecin_id, u.nom, u.prenom, u.email, m.actif, s.nom AS specialite
+            FROM medecins m
+            JOIN users u ON m.id_user = u.id
+            JOIN specialites s ON m.id_specialite = s.id
+            ORDER BY u.id DESC";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 }
